26e97c84135b4a4a044a7d27e257696347d55d79
Lin <hoomjac@gmail.com>
2017-09-25 18:15:50 +0800

    解决拖拉重复生成组件的bug

--- src/js/common/precedure/bind_event.js
+++ src/js/common/precedure/bind_event.js
@@ -8,6 +8,43 @@ import {
   textarea
 } from '../../components/dom/dom'
 
+const bindColEvent = column_str => {
+  const row = $(column_str)
+  const col = row.find('.fd-col')
+  col.on('dragover', function(event) {
+    event.preventDefault()
+    event.stopPropagation()
+    $(this).css('border', '1px solid gold')
+  })
+
+  col.on('dragleave', function(event) {
+    event.preventDefault()
+    event.stopPropagation()
+    $(this).css('border', '1px solid rgba(128, 128, 128, 0.47)')
+  })
+
+  col.on('drop', function(event) {
+    event.preventDefault()
+    event.stopPropagation()
+    const widgetType = event.originalEvent.dataTransfer.getData('type')
+    $(this).css('border', '1px solid rgba(128, 128, 128, 0.47)')
+    switch (widgetType) {
+      case 'text':
+        $(this).append(text)
+        break
+      case 'button':
+        $(this).append(button)
+        break
+      case 'textarea':
+        $(this).append(textarea)
+      default:
+        break
+    }
+  })
+
+  return row
+}
+
 const bindEvent = (widgetClass, canvasId) => {
   const widgets = $(`.${widgetClass}`)
   const canvas = $(`#${canvasId}`)
@@ -34,51 +71,20 @@ const bindEvent = (widgetClass, canvasId) => {
   canvas.on('drop', function(event) {
     event.preventDefault()
     event.stopPropagation()
-    setTimeout(function() {
-      const columns = $('.fd-col')
-      $.each(columns, function(index, column) {
-        let col = $(column)
-        col.on('dragover', function(event) {
-          event.preventDefault()
-          event.stopPropagation()
-          col.css('border', '1px solid gold')
-        })
-        col.on('dragleave', function(event) {
-          event.preventDefault()
-          event.stopPropagation()
-          col.css('border', '1px solid rgba(128, 128, 128, 0.47)')
-        })
-        col.on('drop', function(event) {
-          event.preventDefault()
-          event.stopPropagation()
-          const widgetType = event.originalEvent.dataTransfer.getData('type')
-          col.css('border', '1px solid rgba(128, 128, 128, 0.47)')
-          switch (widgetType) {
-            case 'text':
-              col.append(text)
-              break
-            case 'button':
-              col.append(button)
-              break
-            case 'textarea':
-              col.append(textarea)
-            default:
-              break
-          }
-        })
-      })
-    }, 0)
     canvas.css('border', '3px solid #ffffff')
     const widgetType = event.originalEvent.dataTransfer.getData('type')
     switch (widgetType) {
       case 'column_one':
-        canvas.append(column_one)
+        let columnOne = bindColEvent(column_one)
+        canvas.append(columnOne)
         break
       case 'column_two':
-        canvas.append(column_two)
+        let columnTwo = bindColEvent(column_two)
+        canvas.append(columnTwo)
         break
       case 'column_three':
-        canvas.append(column_three)
+        let columnThree = bindColEvent(column_three)
+        canvas.append(columnThree)
       default:
         break
     }
