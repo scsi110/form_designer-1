c7dc06bad292d7391173e19f62ca0264124d47f0
Lin <hoomjac@gmail.com>
2017-11-29 09:58:20 +0800

    增加textarea组件的验证

--- src/js/components/textarea/index.js
+++ src/js/components/textarea/index.js
@@ -11,6 +11,12 @@ class Textarea extends WidgetBase {
     this.config.placeholder = '请输入文本'
     this.config.defaultValue = undefined
     this.config.maxlength = undefined
+    this.config.readonly = false
+    this.config.validate = {
+      rule: 'norule',
+      customer: '',
+      errMsg: '请输入正确的格式'
+    }
     this.name = 'multiLineInput'
     this.type = 'textarea'
   }
@@ -18,7 +24,11 @@ class Textarea extends WidgetBase {
   // 界面新增元素或元素发生更改重绘时，调用此方法生成元素的DOM（见 modify_dom.js 文件）
   createDOM = () => {
     let { tag, config, attrs } = this
-    let element = $(`<${tag} class="c-field"></${tag}>`)
+    let element = $(
+      `<${tag} class="c-field" ${this.config.readonly ? 'readonly' : ''}></${
+        tag
+      }>`
+    )
 
     Object.keys(attrs).forEach(attr => {
       element.attr(attr, attrs[attr])
@@ -59,7 +69,10 @@ class Textarea extends WidgetBase {
       defaultValue,
       name,
       rows,
-      maxlength
+      maxlength,
+      validate,
+      readonly,
+      required
     } = this.config
     const tpl = `
     <ul class="fd-widget-configs" id="fd-config-list">
@@ -94,7 +107,7 @@ class Textarea extends WidgetBase {
       </li>
       <li class="row fd-config-item">
         <div class="col-xs-24 col-sm-12">
-          <label>行数(文本宽度)</label>
+          <label>行数(文本框宽度)</label>
           <input type="text" class="c-field u-small" data-type="rows" value="${
             rows
           }" />
@@ -106,6 +119,90 @@ class Textarea extends WidgetBase {
           }" />
         </div>
       </li>
+
+      <li class="row fd-config-item">
+        
+        <div class="col-xs-24 col-sm-12">
+            <label>验证规则</label>
+            <select type="text" class="c-field u-small multiline-input-type-select">
+              <option value="norule" ${
+                validate.rule === 'norule' ? 'selected' : ''
+              } data-type="norule">无限制</option>
+
+              <option value="email" ${
+                validate.rule === 'email' ? 'selected' : ''
+              } data-type="email">邮箱</option>
+
+              <option value="number" ${
+                validate.rule === 'number' ? 'selected' : ''
+              } data-type="number">数字</option>
+
+              <option value="url" ${
+                validate.rule === 'url' ? 'selected' : ''
+              } data-type="url">网址</option>
+
+              <option value="cellphone" ${
+                validate.rule === 'cellphone' ? 'selected' : ''
+              } data-type="cellphone">手机号码</option>
+
+              <option value="IDNumber" ${
+                validate.rule === 'IDNumber' ? 'selected' : ''
+              } data-type="IDNumber">身份证</option>
+
+              <option value="IPAddress" ${
+                validate.rule === 'IPAddress' ? 'selected' : ''
+              } data-type="IPAddress">IP地址</option>
+
+              <option value="chinese" ${
+                validate.rule === 'chinese' ? 'selected' : ''
+              } data-type="chinese">中文</option>
+
+              <option value="english" ${
+                validate.rule === 'english' ? 'selected' : ''
+              } data-type="english">英文</option>
+
+              <option value="enAndNum" ${
+                validate.rule === 'enAndNum' ? 'selected' : ''
+              } data-type="enAndNum">英文与数字混合</option>
+
+              <option value="customerRule" ${
+                validate.rule === 'customerRule' ? 'selected' : ''
+              } data-type="customerRule">自定义规则</option>
+              
+            </select>
+          </div>
+
+        <div class="col-xs-24 col-sm-12">
+            <label>自定义规则</label>
+            <input type="text" class="c-field u-small input_customer_reg" placeholder="请输入自定义的正则表达式" ${
+              validate.rule === 'customerRule' ? '' : 'disabled=disabled'
+            }" />
+        </div>
+      </li>
+
+      <li class="fd-config-item row">
+          <div class="ui form field col-xs-12">
+              <label>错误提示</label>
+            <input type="text" class="c-field u-small input_error-message" placeholder="请输入错误提示" value=${
+              validate.errMsg ? validate.errMsg : ''
+            } />
+          </div>
+        </li>
+
+       <li class="fd-config-item row">
+          <div class="ui form field col-xs-12">
+              <label class="c-field c-field--choice">
+                <input type="checkbox" data-type="readonly" ${
+                  readonly ? 'checked' : ''
+                }> 只读
+              </label>
+              <label class="c-field c-field--choice">
+                <input type="checkbox" data-type="required" ${
+                  required ? 'checked' : ''
+                } > 必填项
+              </label>
+          </div>
+        </li>
     </ul>
     `
     return this.bingConfigEvent(tpl)
@@ -117,15 +214,61 @@ class Textarea extends WidgetBase {
     const widgetId = this.id
     const element = $(template)
     const curConfig = this.config
-    const inputs = element.find('input')
-    inputs.on('input', function() {
+    const inputText = element.find('input')
+    const inputCheckbox = element.find('input:checkbox')
+    inputText.on('input', function() {
       const attrName = $(this).data('type')
       let value = $(this).val()
       curConfig[attrName] = value
       self.emitChange()
     })
 
+    inputCheckbox.on('change', function(e) {
+      const $this = $(this)
+      const attrName = $this.data('type')
+      let value = $this.is(':checked') ? true : false
+      curConfig[attrName] = value
+      self.emitChange()
+    })
+
     return element
+  }
+
+  afterConfigPanelInit() {
+    $('.multiline-input-type-select').select2({
+      width: '100%'
+    })
+    const self = this
+    const { configPanelRef } = this
+    const curConfig = this.config
+    const input_select = configPanelRef.find('select')
+    const input_customer_reg = configPanelRef.find('.input_customer_reg')
+    const input_error_message = configPanelRef.find('.input_error-message')
+    input_select.on('change', function() {
+      const $this = $(this)
+      const rule = $this.val()
+      if (rule === 'customerRule') {
+        input_customer_reg.prop('disabled', false)
+      } else {
+        input_customer_reg.prop('disabled', 'disabled')
+      }
+      curConfig.validate.rule = rule
+      self.emitChange()
+    })
+
+    input_customer_reg.on('input', function() {
+      const $this = $(this)
+      const rule = $this.val()
+      curConfig.validate.customer = rule
+      self.emitChange()
+    })
+
+    input_error_message.on('input', function() {
+      const $this = $(this)
+      const msg = $this.val()
+      curConfig.validate.errMsg = msg
+      self.emitChange()
+    })
   }
 }
 
